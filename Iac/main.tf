13820a5c0bc17c6c51a29b0b7b91aae2ba4c062b
William Mian <william_mian@yahoo.com.br>
2021-10-25 17:11:52 -0300

    IaC com Terraform e Ansible

---
+++ Iac/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-09e67e426f25ce0d7"
+  instance_type = "t2.micro"
+  key_name = "iac-alura"
+#   user_data = <<-EOF
+#                  #!/bin/bash
+#                  cd /home/ubuntu
+#                  echo "<h1>Feito com Terraform</h1>" > index.html
+#                  nohup busybox httpd -f -p 8080 &
+#                  EOF
+  tags = {
+    Name = "Teste aws"
+  }
+}
